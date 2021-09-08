2048b06b6d4a2339a56d41ef87833991e74761dc
Renan Marcel Paulino Leite <renannn@outlook.com>
2021-09-07 23:34:11 -0300

    fix: Correção da database

--- dockerfile
+++ dockerfile
@@ -8,13 +8,15 @@ RUN mkdir /var/opt/sqlserver/log
 RUN mkdir /var/opt/sqlserver/backup
 RUN mkdir -p /usr/work
 
+COPY ./scripts/ /usr/work/
+
 RUN chown -R mssql /var/opt/sqlserver
 RUN chmod +x /usr/work/import-data.sh
 
 USER mssql
 
 # enable the agent
-RUN sudo /opt/mssql/bin/mssql-conf set sqlagent.enabled true 
+RUN /opt/mssql/bin/mssql-conf set sqlagent.enabled true 
 
 CMD /usr/work/import-data.sh & /opt/mssql/bin/sqlservr
 
--- scripts/import-data.sh
+++ scripts/import-data.sh
@@ -1,11 +1,10 @@
-SqlPassword="Testing22@@"
 # wait for the SQL Server to come up
 sleep 90s
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SqlPassword -i setup.sql
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SqlPassword -i CreateTables.sql
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SqlPassword -i CreateJobBackupCompleto.sql
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SqlPassword -i CreateJobBackupDiferencial.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Testing22@@ -i /usr/work/setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Testing22@@ -i /usr/work/CreateTables.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Testing22@@ -i /usr/work/CreateJobBackupCompleto.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Testing22@@ -i /usr/work/CreateJobBackupDiferencial.sql
 
 # import the data from the csv file
-/opt/mssql-tools/bin/bcp heroes.dbo.HeroValue in "/usr/work/heroes.csv" -c -t',' -S localhost -U SA -P $SqlPassword
+/opt/mssql-tools/bin/bcp heroes.dbo.HeroValue in "/usr/work/heroes.csv" -c -t',' -S localhost -U SA -P Testing22@@
